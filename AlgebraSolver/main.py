348f22bf578f536758bf47c1d054377bea12c537
IXEXproductions <IXEX_Productions@hotmail.com>
2024-04-15 08:44:14 -0700

    Fixing the "movers" (xy_move() problem unfixed)

--- AlgebraSolver/main.py
+++ AlgebraSolver/main.py
@@ -168,122 +168,73 @@ def exponents(var):
 
 
 def number_mover(var, split_var):
-    # Checks if there are numebers not attatched to a x or y variable on the left side of the "="
-    if len(re.findall(r"\b\d+\b", split_var[0])) > 0:
-        integer_variable = int(re.findall(r'\b(\d+)\b', split_var[0])[0])
-        string_variable = str(re.findall(r'\b(\d+)\b', split_var[0])[0])
-        if len(split_var[1]) == 0:
-            if integer_variable == 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                print(var)
-                # Adds the the value to the other side
-                var = var[var.find('='):] + ' ' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            elif integer_variable != 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                # Adds the opposite of the value to the other side
-                var = var[var.find('='):] + ' -' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            else:
-                error()
-        elif len(split_var[1]) > 0:
-            if integer_variable == 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                # Adds the the value to the other side
-                var = var[var.find('='):] + ' ' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            elif integer_variable != 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                # Adds the opposite of the value to the other side
-                var = var[var.find('='):] + ' - ' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            else:
-                error()
+    if len(re.findall(r'-*(\d+)[^\w]', split_var[0])) > 0:
+        moving_number = re.findall(r'(-*\d+)[^\w]', split_var[0])[0]
+        if '0' in moving_number:
+            # Removes the current value on the left
+            var = var[:var.find(moving_number)] + var[var.find(moving_number) + len(moving_number):]
+            # Adds the the value to the other side
+            var = var[var.find('='):] + ' + ' + moving_number + '' + var[:var.find('=')]
+            return var
         else:
-            error()
+            # Removes the current value on the left
+            var = var[:var.find(moving_number)] + var[var.find(moving_number) + len(moving_number):]
+            # Adds the the value to the other side
+            var = var[var.find('='):] + ' + ' + -int(moving_number) + '' + var[:var.find('=')]
+            return var
 
-    # Checks if there are negative numebers not attatched to a x or y variable on the left side of the "="
-    elif len(re.findall(r"\b-\d+\b", split_var[0])) > 0:
-        if len(split_var[1]) == 0:
-            if integer_variable == 0:
-                error(-0)
-            elif integer_variable != 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                # Adds the opposite of the value to the other side
-                var = var[var.find('='):] + ' -' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            else:
-                error()
-        elif len(split_var[1]) > 0:
-            if integer_variable == 0:
-                error(-0)
-            elif integer_variable != 0:
-                # Removes the current value on the left
-                var = var[:var.find(string_variable)] + var[var.find(string_variable) + len(string_variable):]
-                # Adds the opposite of the value to the other side
-                var = var[var.find('='):] + '+ -' + string_variable + ' ' + var[:var.find('=')]
-                return var
-            else:
-                error()
-        else:
-            error()
-    
     else:
-        #return var
-        pass
+        return var
 
 
-def x_mover(var, split_var):
+def xy_mover(var, split_var):
     x_or_y = r"(\bx\b|\by\b|\bx(?:[^\s]+)y\b|\by(?:[^\s]+)x\b|\bx(?:[^\s]+)x\b|\by(?:[^\s]+)y\b|\bxy\b|\byx\b)"
-    neg_x_or_y = r"(\b-x\b|\b-y\b|\b-x(?:[^\s]+)y\b|\b-y(?:[^\s]+)x\b|\b-x(?:[^\s]+)x\b|\b-y(?:[^\s]+)y\b|\b-xy\b|\b-yx\b)"
+    negative_x_or_y = r"(\b-x\b|\b-y\b|\b-x(?:[^\s]+)y\b|\b-y(?:[^\s]+)x\b|\b-x(?:[^\s]+)x\b|\b-y(?:[^\s]+)y\b|\b-xy\b|\b-yx\b)"
     if len(re.findall(x_or_y, var)) > 0:
         if len(re.findall(x_or_y, split_var[1])) > 0:
             print('looking for x')
             # Checks if "x" is a negative value on the right side of the "="
-            if len(re.findall(neg_x_or_y, re.sub(r"[0-9]", '', split_var[1])))[0]:
+            if len(re.findall(negative_x_or_y, re.sub(r"[0-9]", '', split_var[1])))[0]:
                 if len(split_var[0]) == 0:
-                    # Strings of x value
-                    xy_variable = re.findall(r"(-\b\d*\b)", split_var[1])[0]
+                    xy_string = re.findall(r"(-\b\d\b(?:" + negative_x_or_y + r")", split_var[1])[0]
+                    xy_variable = xy_value = re.findall(r"-\b\d\b" + negative_x_or_y, split_var[1])[0]
+                    xy_value = re.findall(r"-\b(\d)\b(?:" + negative_x_or_y + r")", split_var[1])[0]
                     # Removes the current "x" value on the right
-                    var = var[:var.find(xy_variable)] + ' ' +  var[var.find(xy_variable) + len(xy_variable):]
+                    var = var[:var.find(xy_string)] + '' +  var[var.find(xy_string) + len(xy_string):]
                     # Adds the opposite of the "x" value to the other side
-                    var = var[:var.find('=')] + ' ' + xval + 'x ' + var[var.find('='):]
+                    var = var[:var.find('=')] + '' + xy_value + xy_variable + ' ' + var[var.find('='):]
                     return var
                 
                 elif len(split_var[0]) > 0:
-                    # Strings of x value
-                    x = re.findall(r"(-\b\d*\bx)", split_var[1])[0]
-                    xval = re.findall(r"-\b(\d*)\bx", split_var[1])[0]
+                    xy_string = re.findall(r"(-\b\d\b(?:" + negative_x_or_y + r")", split_var[1])[0]
+                    xy_variable = xy_value = re.findall(r"-\b\d\b" + negative_x_or_y, split_var[1])[0]
+                    xy_value = re.findall(r"-\b(\d)\b(?:" + negative_x_or_y + r")", split_var[1])[0]
                     # Removes the current "x" value on the right
-                    var = var[:var.find(x)] + ' ' +  var[var.find(x) + len(x):]
+                    var = var[:var.find(xy_string)] + ' ' +  var[var.find(xy_string) + len(xy_string):]
                     # Adds the opposite of the "x" value to the other side
-                    var = var[:var.find('=')] + ' + -' + xval + 'x ' + var[var.find('='):]
+                    var = var[:var.find('=')] + ' + ' + xy_value + xy_variable + ' ' + var[var.find('='):]
                     return var
-        
+                
+            # Checks if "x" is a positive value on the right side of the "="
             elif 'x' in split_var[1]:
                 if len(split_var[0]) == 0:
-                    # Strings of x value
-                    x = re.findall(r"(\b\d*\bx)", split_var[1])[0]
-                    xval = re.findall(r"\b(\d*)\bx", split_var[1])[0]
+                    xy_string = re.findall(r"(-\b\d\b(?:" + x_or_y + r")", split_var[1])[0]
+                    xy_variable = xy_value = re.findall(r"-\b\d\b" + x_or_y, split_var[1])[0]
+                    xy_value = re.findall(r"-\b(\d)\b(?:" + x_or_y + r")", split_var[1])[0]
                     # Removes the current "x" value on the right
-                    var = var[:var.find(x)] + ' ' +  var[var.find(x) + len(x):]
+                    var = var[:var.find(xy_string)] + '' +  var[var.find(xy_string) + len(xy_string):]
                     # Adds the opposite of the "x" value to the other side
-                    var = var[:var.find('=')] + ' ' + xval + 'x ' + var[var.find('='):]
+                    var = var[:var.find('=')] + '' + xy_value + xy_variable + ' ' + var[var.find('='):]
                     return var
                 
                 elif len(split_var[0]) > 0:
-                    # Strings of x value
-                    x = re.findall(r"(\b\d*\bx)", split_var[1])[0]
-                    xval = re.findall(r"\b(\d*)\bx", split_var[1])[0]
+                    xy_string = re.findall(r"(-\b\d\b(?:" + x_or_y + r")", split_var[1])[0]
+                    xy_variable = xy_value = re.findall(r"-\b\d\b" + x_or_y, split_var[1])[0]
+                    xy_value = re.findall(r"-\b(\d)\b(?:" + x_or_y + r")", split_var[1])[0]
                     # Removes the current "x" value on the right
-                    var = var[:var.find(x)] + ' ' +  var[var.find(x) + len(x):]
+                    var = var[:var.find(xy_string)] + ' ' +  var[var.find(xy_string) + len(xy_string):]
                     # Adds the opposite of the "x" value to the other side
-                    var = var[:var.find('=')] + ' + -' + xval + 'x ' + var[var.find('='):]
+                    var = var[:var.find('=')] + ' + -' + xy_value + xy_variable + ' ' + var[var.find('='):]
                     return var
                 
                 else:
@@ -293,18 +244,15 @@ def x_mover(var, split_var):
                 return var
 
 
-def solve_x(var, split_var):
+def solve_xy(var, split_var):
     xs = re.findall(r"\b([^\s]*-*\d*)x\b", split_var[0])
     str(xs).replace('(', '').replace(')', '').replace("'", '').replace(',', '')
     print(xs)
 
 
-def solve_xy(var, split_var):
-    pass
-
-
 def solve(var, split_var, original_var):
                 print('\nsolve initiated!\n')
+                print(var)
                 # Solves the equation and returns the value of x and or y
                 parantheses_check = len(re.findall(r'\((.+?)\)', var)) 
                 exponents_check = len(re.findall(r'\s(\*{2})\s', var))
@@ -324,34 +272,25 @@ def solve(var, split_var, original_var):
                         var = division(var)
                         return var
                 
-                if len(re.findall(r'\s*-*\b(\d+)\b\s*', split_var[1])) > 0:
+                if len(re.findall(r'\s*-*\b(\d+)\b\s*', split_var[0])) != 0:
                     var = number_mover(var, split_var)
                     return var
 
 
-                elif len(re.findall(r'\s*-*\b(\d+)\b\s', split_var[0])) != len(split_var[0]):
-                    var = x_mover(var, split_var)
-                    #x_y_mover(var, split_var)
-                    ssv = str(eval(split_var[0]))
-                    var = var[:var.find(split_var[0])] + ssv +  var[var.find(split_var[0]) + len(split_var[0]):]
+                elif len(re.findall(r'(x|y)', split_var[1])) > 0:
+                    var = xy_mover(var, split_var)
                     return var
                 
 
-                elif len(re.findall('x', split_var[1])) and len(re.findall('y', split_var[1])) == 0:
+                elif len(re.findall('x', split_var[1])) == 0 and len(re.findall('y', split_var[1])) == 0 and len(re.findall('x', split_var[0])) != 1 and len(re.findall('y', split_var[0])) != 1:
                     print(len(re.findall('y', split_var[1])))
                     print(len(re.findall('x', split_var[1])))
-                    if 'x' and 'y' in var:
-                        solve_x(var, split_var)
-                        return var
-                    elif 'x' in var:
-                        solve_xy(var, split_var)
-                        return var
-                    else:
-                        pass
+                    var = solve_xy(var, split_var)
+                    return var
 
 
                 else:
-                    os.system('clear')
+                    print(split_var)
                     print(f'The solution of {original_var}\n{split_var[1]} = {split_var[0]}\n')
                     running = False
                     exit(0)
@@ -362,7 +301,6 @@ def main():
     eq = input('>').lower()
     original_eq = str(eq)
     time.sleep(1)
-    numbers = r"[0-9]"
     running = True
     while running:
         time.sleep(1)
@@ -376,17 +314,16 @@ def main():
 
 
         elif 'quit' == eq.lower():
-            os.system('clear')
+            
             exit(0)
 
         else:
             # Executes When There Is An Error With Formatting
             print_statment = '''\nPlease follow the correct formatting structure. 
-            If your equation has only one variable, make sure the variable is set to "x". 
-            If it is a two variable equation set one variable to "x" and one to "y".
-            There cannot be a space inbetween the chevron and your equation.
-            If you wish to exit the program type "quit".
-            Resetting program...'''
+            \nIf your equation has only one variable, make sure the variable is set to "x". 
+            \nIf it is a two variable equation set one variable to "x" and one to "y".
+            \nThere cannot be a space inbetween the chevron and your equation.
+            \nPress Enter to reset program'''
             input(print_statment)
             main()
             exit(1)
